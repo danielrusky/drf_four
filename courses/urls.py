2e28821183865c349d636c4ac1f0a4ba23c179b2
danila <spook9400@mail.ru>
2024-04-19 11:51:36 +0300

    add homework 25.2 Валидаторы, пагинация и тесты

--- courses/urls.py
+++ courses/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from courses.apps import CoursesConfig
-from courses.views import CoursesViewSet, LessonListCreateAPIView, LessonRetrieveAPIView, LessonCreateAPIView, \
-    LessonUpdateAPIView, LessonDestroyAPIView, SubscriptionAPIView
+from courses.views import CoursesViewSet, LessonRetrieveAPIView, LessonCreateAPIView, \
+    LessonUpdateAPIView, LessonDestroyAPIView, SubscriptionAPIView, LessonListAPIView
 
 app_name = CoursesConfig.name
 
@@ -13,7 +13,7 @@ router.register(r'courses', CoursesViewSet, basename='courses')
 
 urlpatterns = [
     # URL-маршруты для уроков
-    path('lessons/', LessonListCreateAPIView.as_view(), name='lesson-list-create'),
+    path('lessons/', LessonListAPIView.as_view(), name='lesson-list-create'),
     path('lessons/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson-retrieve'),
     path('lessons/create/', LessonCreateAPIView.as_view(), name='lesson-create'),
     path('lessons/<int:pk>/update/', LessonUpdateAPIView.as_view(), name='lesson-update'),
--- courses/views.py
+++ courses/views.py
@@ -1,9 +1,12 @@
 from users.permissions import IsOwner, IsModerator, IsNotModerator
-from .models import Lesson
+from .models import Lesson, Subscription
 from .paginators import MyPagination
 from .serializers import CourseSerializer, LessonSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics, permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404
 
 
 # Представления для курсов
